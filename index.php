e0350c2b0e3f448377c6d8d37bc10796e1d3df89
Luke Gripenberg <luke.gripenberg@opsdis.com>
2019-09-02 13:24:41 +0000

    Add mapping array for attribute matching.

--- index.php
+++ index.php
@@ -4,48 +4,64 @@
 <!--
 
 <?php
-foreach($_SERVER as $key=>$value) {
-  if(substr($key, 0, 7) == 'MELLON_') {
-    echo($key . '=' . $value . "\r\n");
+
+$debug = false;
+
+$mapping['http://schemas_microsoft_com/ws/2008/06/identity/claims/role']['match']='admins';
+$mapping['http://schemas_microsoft_com/ws/2008/06/identity/claims/role']['sql']="INSERT INTO icingaweb_group_membership (group_id, username) VALUES ('1', '".$_SERVER['MELLON_NAME_ID']."')";
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "icingaweb2";
+
+
+if ($debug == true) {
+
+    foreach($_SERVER as $key=>$value) {
+
+      if(substr($key, 0, 7) == 'MELLON_') {
+
+        echo($key . '=' . $value . "\r\n");
+
+      }
+
+    }
+
+}
+
+foreach($_SERVER as $key => $value) {
+
+  if(substr($key, 0, 7) == 'MELLON_' && isset($mapping[substr($key,7)])) {
+
+      $mellon_variable = substr($key,7);
+      echo($mellon_variable."\n");
+
+      if (isset($mapping[$mellon_variable])) {
+
+        if ($mapping[$mellon_variable]['match']==$value) {
+
+            echo 'matched:'.$mellon_variable."\n ==".$value;
+            $sql = $mapping[$mellon_variable]['sql'];
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->query($sql) === TRUE) {
+
+                if ($debug == true) { echo "New record created successfully"; }
+
+            } else {
+
+                  if ($debug == true) { echo "Error: " . $sql . "<br>" . $conn->error; }
+
+         }
+      }
+     }
   }
 }
-
-if (isset($_SERVER['MELLON_http://schemas_microsoft_com/ws/2008/06/identity/claims/role'])) { echo($_SERVER['MELLON_http://schemas_microsoft_com/ws/2008/06/identity/claims/role']);
-
-        $servername = "10.0.0.1";
-        $username = "icingaweb2";
-        $password = "xxxxxxxxxxxxx";
-        $dbname = "icingaweb2";
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        $sql = "INSERT INTO icingaweb_group_membership (group_id, username) VALUES ('1', '".$_SERVER['MELLON_NAME_ID']."')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        $conn->close();
-
-}
-?>
-
-
-
-
-
 
 
 
 ?>-->
-<meta http-equiv="Refresh" content="0; url=/icingaweb2">
 </head>
 <body>
 You are now logged in and should be automatically redirected to: <a href="/icingaweb2/">Icingaweb2</a>
